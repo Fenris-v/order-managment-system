f0749fdb476b1875da32f8081ea3e3d3cf7b1a83
Anatoliy Vasilenko <anatolii@wemakefab.ru>
2024-08-06 23:56:17 +0300

    Реализован gRPC-сервер для общения между микросервисами. Реализовано получение пользователя по id или jwt

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.protobuf.gradle.id
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -6,6 +8,8 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.serialization") version "2.0.0"
+
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.example.gateway"
@@ -23,6 +27,13 @@ val versionCatalog = project.rootProject.extensions.getByType<VersionCatalogsExt
 println("Library aliases: ${versionCatalog.bundleAliases}")
 
 dependencies {
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
+    implementation("io.grpc:grpc-all:1.65.1")
+
+    implementation("com.google.protobuf:protobuf-java:4.27.3")
+
     implementation(project(":starter-utils")) // TODO: заменить на nexus
 //    versionCatalog.findLibrary("starterUtils").ifPresent { implementation(it) }
 
@@ -46,16 +57,17 @@ dependencies {
 
     versionCatalog.findLibrary("caffeine").ifPresent { implementation(it) }
 
-    versionCatalog.findBundle("monitoring").ifPresent { implementation(it) }
+//    versionCatalog.findBundle("monitoring").ifPresent { implementation(it) }
     versionCatalog.findBundle("logs").ifPresent { implementation(it) }
 
     versionCatalog.findBundle("test").ifPresent { testImplementation(it) }
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+    dependsOn("generateProto")
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
@@ -65,4 +77,30 @@ tasks.withType<Test> {
 
 tasks.bootJar {
     archiveFileName.set("gateway.jar")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:4.27.3"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.65.1"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("../proto")
+        }
+    }
 }
